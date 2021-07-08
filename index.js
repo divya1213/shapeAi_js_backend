61bbd5ab15e7a9804c0191f1acb75ebc6a1d42a1
divya1213 <86291368+divya1213@users.noreply.github.com>
2021-07-08 07:26:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 8080;
 
-app.unescape(bodyParser.urlencoded({extented: true}));
+app.use(bodyParser.urlencoded({extented: true}));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname+"/index.html");
 });
 app.post("/",function(req,res){
-  console.log(red.body.n1);
-  res.send("This is a successful post");
+ var age = (Number(req.body.n1)/15);
+  res.send("Your age in Dog Years:" + age + "dog years.");
 });
 
 
